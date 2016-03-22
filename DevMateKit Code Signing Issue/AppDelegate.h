13d1c9c1ea9c4db180c1188a18eb80a6520cfb46
Boy van Amstel <boyvanamstel@Boys-Mac-mini.local>
2016-03-22 11:43:53 +0100

    Initial import

---
+++ DevMateKit Code Signing Issue/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DevMateKit Code Signing Issue
+//
+//  Created by Boy van Amstel on 22/03/16.
+//  Copyright © 2016 The Sync Factory. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ DevMateKit Code Signing Issue/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  DevMateKit Code Signing Issue
+//
+//  Created by Boy van Amstel on 22/03/16.
+//  Copyright © 2016 The Sync Factory. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+  return NSApplicationMain(argc, argv);
+}
